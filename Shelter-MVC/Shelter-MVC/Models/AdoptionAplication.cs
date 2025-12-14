522e68026b190e0ba307f8234d33d584c603e745
Ewelina <94842711+EwelinaBul928928@users.noreply.github.com>
2025-12-14 19:28:56 +0100

    Add files via upload

---
+++ Shelter-MVC/Shelter-MVC/Models/AdoptionAplication.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Shelter_MVC.Models
+{
+    public enum ApplicationStatus
+    {
+        Oczekujący, Zaakceptowany, Odrzucony
+    }
+
+    public class AdoptionApplication
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Data zgłoszenia")]
+        public DateTime ApplicationDate { get; set; } = DateTime.Now;
+
+        [Required(ErrorMessage = "Napisz dlaczego chcesz adoptować")]
+        [Display(Name = "Wiadomość do schroniska")]
+        public string Message { get; set; }
+
+        public ApplicationStatus Status { get; set; } = ApplicationStatus.Oczekujący;
+
+        public string? UserId { get; set; }
+
+        public int AnimalId { get; set; }
+
+        [ForeignKey("AnimalId")]
+        public virtual Animal? Animal { get; set; }
+    }
+}
---
+++ Shelter-MVC/Shelter-MVC/Models/HealthRecord.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Shelter_MVC.Models
+{
+    public class HealthRecord
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Data wizyty")]
+        [DataType(DataType.Date)]
+        public DateTime VisitDate { get; set; }
+
+        [Required(ErrorMessage = "Opis zabiegu jest wymagany")]
+        [Display(Name = "Opis zabiegu/szczepienia")]
+        public string Description { get; set; }
+
+        [Display(Name = "Weterynarz")]
+        public string? VetName { get; set; }
+
+        public int AnimalId { get; set; }
+
+        [ForeignKey("AnimalId")]
+        public virtual Animal? Animal { get; set; }
+    }
+}
