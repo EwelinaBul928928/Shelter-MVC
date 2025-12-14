426949b99a770128a2f54cbf82c389b73ef8bd58
Ewelina <94842711+EwelinaBul928928@users.noreply.github.com>
2025-12-14 19:33:22 +0100

    Dodaj pliki projektów.

---
+++ Shelter-MVC/Models/Animal.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Shelter_MVC.Models
+{
+    public class Animal
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Imię jest wymagane")]
+        [Display(Name = "Imię zwierzaka")]
+        public string Name { get; set; }
+
+        [Display(Name = "Wiek (w latach)")]
+        [Range(0, 30, ErrorMessage = "Wiek musi być między 0 a 30 lat")]
+        public int Age { get; set; }
+
+        [Display(Name = "Opis")]
+        public string? Description { get; set; }
+
+        [Display(Name = "Zdjęcie (URL)")]
+        public string? PhotoUrl { get; set; }
+
+        [Display(Name = "Czy zaadoptowany?")]
+        public bool IsAdopted { get; set; } = false;
+
+        [Display(Name = "Gatunek")]
+        public int SpeciesId { get; set; }
+
+        [ForeignKey("SpeciesId")]
+        public virtual Species? Species { get; set; }
+
+        public virtual ICollection<HealthRecord>? HealthRecords { get; set; }
+    }
+}
---
+++ Shelter-MVC/Models/Species.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shelter_MVC.Models 
+{
+    public class Species
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Podaj nazwę gatunku")]
+        [Display(Name = "Gatunek")]
+        public string Name { get; set; }
+
+        public virtual ICollection<Animal>? Animals { get; set; }
+    }
+}
